da43932f6be76b3b07b971a05365eff6359cd30e
Devin Hight <dhightnm@gmail.com>
2021-07-30 23:20:38 -0700

    Added basic Nav bar

--- public/components/layout.js
+++ public/components/layout.js
@@ -1,3 +1,4 @@
+import Nav from './Nav'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from './layout.module.css'
@@ -9,22 +10,9 @@ export const siteTitle = "Devin's Portfolio"
 
 export default function Layout({ children, home }) {
   return (
+    <>
     <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
+      <Nav />
       <header className={styles.header}>
         {home ? (
           <>
@@ -69,5 +57,6 @@ export default function Layout({ children, home }) {
         </div>
       )}
     </div>
+    </>
   )
 }
